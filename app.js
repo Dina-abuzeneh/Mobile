902d3aa3570258e44bb8ccb5f955a144dc9b4888
dinamous123 <>
2021-06-02 13:46:43 +0000

    jsfile

---
+++ app.js
@@ -0,0 +1,12 @@
+var firstName="Dina"
+var lastName="mousa"
+var finalName=firstName + lastName
+console.log(finalName)
+
+var x=3
+var y=7
+if(x<y){document.write('<h1>'+"hello"+'</h1>')}
+else{document.write('<h1>'+"welcome"+'</h1>')}
+
+alert('welcom')
+prompt("what is your name?")
