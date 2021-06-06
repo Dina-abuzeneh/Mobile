f53e185923c31f09668c2e4e8eb172a15befc29c
dinamous123 <>
2021-06-06 12:57:28 +0000

    change in css file

--- app.js
+++ app.js
@@ -3,16 +3,6 @@ var lastName="mousa"
 var finalName=firstName + lastName
 console.log(finalName)
 
-var x=3
-var y=7
-if(x<y){document.write('<h1>'+"hello"+'</h1>')}
-else{document.write('<h1>'+"welcome"+'</h1>')}
-
-alert('welcom')
-prompt("what is your name?")
-
-
-
 
 
 function welcoming(){
